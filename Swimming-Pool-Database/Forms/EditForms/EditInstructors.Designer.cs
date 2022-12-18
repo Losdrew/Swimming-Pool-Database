d58c092aff7d1dbc989cc44b0fe8de7eb78d6a22
Artem Volovyi <artem.volovyi@nure.ua>
2022-12-18 12:46:19 +0200

    Add EditInstructors form

---
+++ Swimming-Pool-Database/Forms/EditForms/EditInstructors.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Swimming_Pool_Database.Forms
+{
+    partial class EditInstructors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label loginLabel;
+            System.Windows.Forms.Label emailLabel;
+            System.Windows.Forms.Label firstNameLabel;
+            System.Windows.Forms.Label lastNameLabel;
+            System.Windows.Forms.Label middleNameLabel;
+            System.Windows.Forms.Label preparationLevelLabel;
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.poolComboBox = new System.Windows.Forms.ComboBox();
+            this.poolsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.swimmingpoolDataSet = new Swimming_Pool_Database.swimmingpoolDataSet();
+            this.middleNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.poolsTableAdapter = new Swimming_Pool_Database.swimmingpoolDataSetTableAdapters.PoolsTableAdapter();
+            this.instructorsTableAdapter = new Swimming_Pool_Database.swimmingpoolDataSetTableAdapters.InstructorsTableAdapter();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            loginLabel = new System.Windows.Forms.Label();
+            emailLabel = new System.Windows.Forms.Label();
+            firstNameLabel = new System.Windows.Forms.Label();
+            lastNameLabel = new System.Windows.Forms.Label();
+            middleNameLabel = new System.Windows.Forms.Label();
+            preparationLevelLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.poolsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.swimmingpoolDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginLabel
+            // 
+            loginLabel.AutoSize = true;
+            loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            loginLabel.Location = new System.Drawing.Point(40, 134);
+            loginLabel.Name = "loginLabel";
+            loginLabel.Size = new System.Drawing.Size(107, 25);
+            loginLabel.TabIndex = 55;
+            loginLabel.Text = "Зарплата:";
+            // 
+            // emailLabel
+            // 
+            emailLabel.AutoSize = true;
+            emailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            emailLabel.Location = new System.Drawing.Point(40, 201);
+            emailLabel.Name = "emailLabel";
+            emailLabel.Size = new System.Drawing.Size(199, 25);
+            emailLabel.TabIndex = 53;
+            emailLabel.Text = "Електронна адреса:";
+            // 
+            // firstNameLabel
+            // 
+            firstNameLabel.AutoSize = true;
+            firstNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            firstNameLabel.Location = new System.Drawing.Point(40, 35);
+            firstNameLabel.Name = "firstNameLabel";
+            firstNameLabel.Size = new System.Drawing.Size(55, 25);
+            firstNameLabel.TabIndex = 39;
+            firstNameLabel.Text = "Ім\'я:";
+            // 
+            // lastNameLabel
+            // 
+            lastNameLabel.AutoSize = true;
+            lastNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            lastNameLabel.Location = new System.Drawing.Point(40, 68);
+            lastNameLabel.Name = "lastNameLabel";
+            lastNameLabel.Size = new System.Drawing.Size(107, 25);
+            lastNameLabel.TabIndex = 40;
+            lastNameLabel.Text = "Прізвище:";
+            // 
+            // middleNameLabel
+            // 
+            middleNameLabel.AutoSize = true;
+            middleNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            middleNameLabel.Location = new System.Drawing.Point(40, 100);
+            middleNameLabel.Name = "middleNameLabel";
+            middleNameLabel.Size = new System.Drawing.Size(128, 25);
+            middleNameLabel.TabIndex = 41;
+            middleNameLabel.Text = "По-батькові:";
+            // 
+            // preparationLevelLabel
+            // 
+            preparationLevelLabel.AutoSize = true;
+            preparationLevelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            preparationLevelLabel.Location = new System.Drawing.Point(40, 168);
+            preparationLevelLabel.Name = "preparationLevelLabel";
+            preparationLevelLabel.Size = new System.Drawing.Size(160, 25);
+            preparationLevelLabel.TabIndex = 44;
+            preparationLevelLabel.Text = "Номер басейна:";
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.salaryTextBox.Location = new System.Drawing.Point(255, 134);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(193, 27);
+            this.salaryTextBox.TabIndex = 56;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.emailTextBox.Location = new System.Drawing.Point(255, 201);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(193, 27);
+            this.emailTextBox.TabIndex = 54;
+            this.emailTextBox.Validating += new System.ComponentModel.CancelEventHandler(this.EmailTextBox_Validating);
+            this.emailTextBox.Validated += new System.EventHandler(this.EmailTextBox_Validated);
+            // 
+            // poolComboBox
+            // 
+            this.poolComboBox.DataSource = this.poolsBindingSource;
+            this.poolComboBox.DisplayMember = "pool_id";
+            this.poolComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.poolComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.poolComboBox.FormattingEnabled = true;
+            this.poolComboBox.Location = new System.Drawing.Point(255, 167);
+            this.poolComboBox.Name = "poolComboBox";
+            this.poolComboBox.Size = new System.Drawing.Size(193, 28);
+            this.poolComboBox.TabIndex = 52;
+            this.poolComboBox.ValueMember = "pool_id";
+            // 
+            // poolsBindingSource
+            // 
+            this.poolsBindingSource.DataMember = "Pools";
+            this.poolsBindingSource.DataSource = this.swimmingpoolDataSet;
+            // 
+            // swimmingpoolDataSet
+            // 
+            this.swimmingpoolDataSet.DataSetName = "swimmingpoolDataSet";
+            this.swimmingpoolDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // middleNameTextBox
+            // 
+            this.middleNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.middleNameTextBox.Location = new System.Drawing.Point(255, 101);
+            this.middleNameTextBox.Name = "middleNameTextBox";
+            this.middleNameTextBox.Size = new System.Drawing.Size(193, 27);
+            this.middleNameTextBox.TabIndex = 49;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lastNameTextBox.Location = new System.Drawing.Point(255, 68);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(193, 27);
+            this.lastNameTextBox.TabIndex = 48;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.firstNameTextBox.Location = new System.Drawing.Point(255, 35);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(193, 27);
+            this.firstNameTextBox.TabIndex = 47;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelButton.Location = new System.Drawing.Point(255, 264);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(160, 39);
+            this.cancelButton.TabIndex = 46;
+            this.cancelButton.Text = "Відмінити";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.acceptButton.Location = new System.Drawing.Point(73, 264);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(158, 39);
+            this.acceptButton.TabIndex = 45;
+            this.acceptButton.Text = "Підтвердити";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.AcceptButton_Click);
+            // 
+            // poolsTableAdapter
+            // 
+            this.poolsTableAdapter.ClearBeforeFill = true;
+            // 
+            // instructorsTableAdapter
+            // 
+            this.instructorsTableAdapter.ClearBeforeFill = true;
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // EditInstructors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 342);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(loginLabel);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(emailLabel);
+            this.Controls.Add(this.poolComboBox);
+            this.Controls.Add(this.middleNameTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(firstNameLabel);
+            this.Controls.Add(lastNameLabel);
+            this.Controls.Add(middleNameLabel);
+            this.Controls.Add(preparationLevelLabel);
+            this.Name = "EditInstructors";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Редагування інструктора";
+            ((System.ComponentModel.ISupportInitialize)(this.poolsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.swimmingpoolDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.ComboBox poolComboBox;
+        private System.Windows.Forms.TextBox middleNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button acceptButton;
+        private swimmingpoolDataSet swimmingpoolDataSet;
+        private System.Windows.Forms.BindingSource poolsBindingSource;
+        private swimmingpoolDataSetTableAdapters.PoolsTableAdapter poolsTableAdapter;
+        private swimmingpoolDataSetTableAdapters.InstructorsTableAdapter instructorsTableAdapter;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
---
+++ Swimming-Pool-Database/Forms/EditForms/EditInstructors.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Swimming_Pool_Database.Forms
+{
+    public partial class EditInstructors : Form
+    {
+        private readonly int _id;
+        private readonly bool _isEdit;
+
+        public EditInstructors()
+        {
+            InitializeComponent();
+            poolsTableAdapter.Fill(swimmingpoolDataSet.Pools);
+        }
+
+        public EditInstructors(int id, string firstName, string lastName, string middleName, 
+            string salary, int poolId, string email) : this()
+        {
+            _isEdit = true;
+            _id = id;
+
+            firstNameTextBox.Text = firstName;
+            lastNameTextBox.Text = lastName;
+            middleNameTextBox.Text = middleName;
+            salaryTextBox.Text = salary;
+            poolComboBox.SelectedValue = poolId;
+            emailTextBox.Text = email;
+        }
+
+        private void AcceptButton_Click(object sender, EventArgs e)
+        {
+            if (_isEdit)
+            {
+                if (!CommonFunctions.TryQuery(() =>
+                        instructorsTableAdapter.UpdateQuery(
+                            firstNameTextBox.Text,
+                            lastNameTextBox.Text,
+                            middleNameTextBox.Text,
+                            Convert.ToDecimal(salaryTextBox.Text),
+                            Convert.ToInt32(poolComboBox.SelectedValue),
+                            emailTextBox.Text,
+                            _id)))
+                {
+                    return;
+                }
+            }
+            else
+            {
+                if (!CommonFunctions.TryQuery(() =>
+                        instructorsTableAdapter.Insert(
+                            firstNameTextBox.Text,
+                            lastNameTextBox.Text,
+                            middleNameTextBox.Text,
+                            Convert.ToDecimal(salaryTextBox.Text),
+                            Convert.ToInt32(poolComboBox.SelectedValue),
+                            emailTextBox.Text)))
+                {
+                    return;
+                }
+            }
+
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void EmailTextBox_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!CommonFunctions.IsValidEmail(emailTextBox.Text, out var errorMessage))
+            {
+                e.Cancel = true;
+                emailTextBox.Select(0, emailTextBox.Text.Length);
+                errorProvider.SetError(emailTextBox, errorMessage);
+            }
+        }
+
+        private void EmailTextBox_Validated(object sender, EventArgs e)
+        {
+            errorProvider.SetError(emailTextBox, "");
+        }
+    }
+}
